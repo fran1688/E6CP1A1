70ee0d0e7c32a41d000689aa8be0b43501317215
Francisco Castillo <fcastillo1688@gmail.com>
2017-08-21 21:44:23 -0300

    se modifico el ejercicio 3 segunda carpeta

--- 2 ciclos/ejercicio3.rb
+++ 2 ciclos/ejercicio3.rb
@@ -1,2 +1,22 @@
 # Mostrar todos los divisores del número 990 con:
 # while, for, times.
+
+## times
+
+990.times do |i|	
+	i += 1
+	puts i if (990 % i).zero?
+end
+
+
+## for
+for i in 1..990
+	puts i if (990 % i).zero?
+end
+
+## while
+i = 0
+while i < 990	
+	i += 1
+	puts i if (990 % i).zero?
+end
