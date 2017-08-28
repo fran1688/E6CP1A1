7b67c35e8e42adc7784f7c9908b605422333aac3
Francisco Castillo <fcastillo1688@gmail.com>
2017-08-28 09:14:59 -0300

    Se modifico el ejercicio 8

--- 2 ciclos/ejercicio8.rb
+++ 2 ciclos/ejercicio8.rb
@@ -1,9 +1,11 @@
 # Generar -utilizando un ciclo iterativo- un string con la siguiente estructura:
 # a = '1impar 2par 3impar 4par 5impar 6par 7impar 8par 9impar 10par'
 
-a = ''
 10.times do |i|
-	
+	print i + 1
+  if i.even?
+  	print 'par '
+    else 
+    	print 'impar '
+  end
 end
-
-puts a
